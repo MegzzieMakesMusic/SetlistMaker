d1a03823210068ac878326d670e667de45f995f5
Bret Williams <bretw84@gmail.com>
2022-10-08 12:03:11 -0500

    Added my newest songs to the array

--- js/setlist.js
+++ js/setlist.js
@@ -1,6 +1,4 @@
 $('document').ready(function() {
-    // var json = $.getJSON("./songs.json");
-    // var songs = eval("(" +json.responseText + ")");
     var songs = {
         "song":[
             {
@@ -14,6 +12,72 @@ $('document').ready(function() {
                 "Key":"C Minor",
                 "Length":"4:30",
                 "Genre":"Rock"
+            },
+            {
+                "Title":"Rage",
+                "Key":"G",
+                "Length":"4:54",
+                "Genre":"Rock"
+            },
+            {
+                "Title":"Cool Cat",
+                "Key":"B",
+                "Length":"3:01",
+                "Genre":"Pop"
+            },
+            {
+                "Title":"Honey Brown",
+                "Key":"A",
+                "Length":"4:12",
+                "Genre":"Rock"
+            },
+            {
+                "Title":"High Vibe Information",
+                "Key":"A Minor",
+                "Length":"2:22",
+                "Genre":"Pop"
+            },
+            {
+                "Title":"The Abyss",
+                "Key":"G Minor",
+                "Length":"3:19",
+                "Genre":"Pop"
+            },
+            {
+                "Title":"Block That Bitch",
+                "Key":"D",
+                "Length":"3:33",
+                "Genre":"Pop"
+            },
+            {
+                "Title":"Do You Feel?",
+                "Key":"C",
+                "Length":"3:41",
+                "Genre":"Pop"
+            },
+            {
+                "Title":"We Got The Groove",
+                "Key":"B Minor",
+                "Length":"2:34",
+                "Genre":"Pop"
+            },
+            {
+                "Title":"Barely Holding On",
+                "Key":"C Minor",
+                "Length":"4:01",
+                "Genre":"Ballad"
+            },
+            {
+                "Title":"Hope",
+                "Key":"F",
+                "Length":"2:55",
+                "Genre":"Pop"
+            },
+            {
+                "Title":"Pop Sugar",
+                "Key":"F",
+                "Length":"3:35",
+                "Genre":"Pop"
             }
     
         ]
